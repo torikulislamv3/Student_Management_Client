1f7efb07034e44fe151b24fe68c916fcf5a46a41
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2024-09-05 00:44:02 +0600

    footer section added

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center bg-gray-400 text-primary-content pt-10 pl-10 pr-10">
+  <aside>
+   <img className='rounded-full size-14' src="https://t3.ftcdn.net/jpg/05/08/80/36/360_F_508803603_V6Rt0NOVRXhsld8LGIQb97EWUPvBmWAK.jpg" alt="" />
+    <p className="font-bold">
+      <strong>Rangpur Polytechnic Institute</strong>
+      <br />
+      <span className='text-blue-900'>Computer Science & Technology</span>
+    </p>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+  </aside>
+  <nav>
+    <div>
+        <a className='mr-5 underline text-[16px]' href="https://rangpur.polytech.gov.bd/">About us</a>
+        <a className='mr-5 underline text-[16px]' href="https://rangpur.polytech.gov.bd/site/view/notices">Notice</a>
+    </div>
+    <p className='text-[10px]'>
+        Made by : <a className='underline text-green-600' href="https://www.facebook.com/mdtorikulislamtamim/">MD TORIKUL ISLAM</a>
+    </p>
+  </nav>
+  
+</footer>
+  )
+}
+
+export default Footer
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-   <div className="navbar bg-base-100">
+   <div className="navbar bg-gray-400">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
