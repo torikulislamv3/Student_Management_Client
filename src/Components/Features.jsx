7b34af3306a50632bf4afa6c85d2bd2647fdfe44
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2024-09-07 00:06:55 +0600

    featured item card designed

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -8,25 +8,37 @@ const Features = () => {
         .then((res) => res.json())
         .then((data) => {
             setFeatures(data.sectors);
-            console.log(data.sectors);  // Log the fetched data here directly
+            console.log(data.sectors); 
         })
         .catch((error) => console.error('Error fetching data:', error));
     }, []);
 
     return (
         <>
-            <div className='mt-5'>
+            <div className='mt-5 mb-5'>
                 <marquee className='text-2xl font-bold border bg-gray-600 text-yellow-300'>CST Department Special Features</marquee>
             </div>
-            <section>
+            <section className='mb-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5'>
                 {feature && feature.length > 0 && feature.map((item, index) => (
-                    <div key={index}>
-                        <h2>{item.name}</h2>
-                        <img src={item.img} alt={item.name} />
-                        <p>{item.description}</p>
+                    <div key={index} className="card bg-gray-600 w-96 shadow-xl">
+                    <figure className="px-10 pt-10">
+                      <img
+                        src={item.img}
+                        alt="photo.."
+                        className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                      <h2 className="card-title text-yellow-300">{item.name}</h2>
+                      <p className='text-green-300'>{item.description}</p>
+                      {/* <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                      </div> */}
                     </div>
+                  </div>
+                    
                 ))}
             </section>
+          
         </>
     );
 };
